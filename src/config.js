611e2cd7beeec6c626767b7d4209d92ba14eed0a
ashen-develops <ashenp.designs@gmail.com>
2021-01-10 01:38:29 -0600

    changed from dev to prod

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || 'production',
   DB_URL: process.env.DB_URL
     || 'postgresql://postgres@localhost/spaced-repetition',
   JWT_SECRET: process.env.JWT_SECRET || 'secret',
