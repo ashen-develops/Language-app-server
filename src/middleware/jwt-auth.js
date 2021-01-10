0291cf533c7b38d0fa5b05843acc8903129f8d7e
ashen-develops <ashenp.designs@gmail.com>
2021-01-09 23:05:24 -0600

    set auth and jwt back to beginning

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,8 +1,9 @@
+  
 const { JsonWebTokenError } = require('jsonwebtoken')
 const AuthService = require('../auth/auth-service')
 
 async function requireAuth(req, res, next) {
-  const authToken = req.get('bearer') || ''
+  const authToken = req.get('Authorization') || ''
 
   let bearerToken
   if (!authToken.toLowerCase().startsWith('bearer ')) {
