e0680f6bd3edcee58ca6943fccb7562e2ae37561
ashen-develops <ashenp.designs@gmail.com>
2021-01-12 19:51:41 -0600

    change

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -3,7 +3,7 @@
 const { NODE_ENV } = require('../config');
 
 module.exports = function errorHandler(error, req, res, next) {
-  const response = (NODE_ENV === 'production')
+  const response = (NODE_ENV != 'production')
     ? { error: 'Server error' }
     : (console.error(error), { error: error.message, details: error });
 
